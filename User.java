a7480ca5c5925d7b15f0321e6b207e679269d9f6
sw02020 <91944457+sw02020@users.noreply.github.com>
2021-12-04 12:36:31 +0900

    Delete User.java

--- User.java
+++
@@ -1,8 +0,0 @@
-
-public class User extends Player{
-	public User(String n, Card[] cs, int l , MainGameFrame f, CardButton[] bts, OneCard g) {
-		this.name = n;
-		this.cards = cs;
-		this.len = l;
-	}
-}
