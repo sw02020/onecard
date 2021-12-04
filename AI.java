143d21a8ee0b970645feb98736fb20dc89333017
sw02020 <91944457+sw02020@users.noreply.github.com>
2021-12-04 12:35:00 +0900

    Delete AI.java

--- AI.java
+++
@@ -1,9 +0,0 @@
-
-public class AI extends Player {
-	public AI(Card[] cs, int l, MainGameFrame f, CardButton[] bts, OneCard g) {
-		this.cards = cs;
-	}
-	public void play() {
-		//현재 상황에 맞춰 카드 내기
-	}
-}
